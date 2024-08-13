2fa468b3e59bff7766c29c40a665deed0b66a502
Junnos  <junnos.dev@gmail.com>
2024-08-13 13:39:25 +0900

    ✨ Added body for Dripper to default-implement `pour` method
    Powered by `@resultBuilder`

--- Sources/Dripper/Dripper.swift
+++ Sources/Dripper/Dripper.swift
@@ -8,6 +8,17 @@
 public protocol Dripper<State, Action> {
     associatedtype State
     associatedtype Action
+    associatedtype Body
 
     func pour(_ state: State, _ action: Action) -> State
+
+    @DripperBuilder<State, Action>
+    var body: Body { get }
+}
+
+extension Dripper where Body: Dripper<State, Action> {
+    @inlinable
+    public func pour(_ state: State, _ action: Action) -> State {
+        body.pour(state, action)
+    }
 }
